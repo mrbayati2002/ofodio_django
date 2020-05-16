cd048f9bb6f8608adb3a453adcc8c180d17308cb
mrbayati2002 <mrbayati2002@gmail.com>
2020-05-16 22:45:39 +0430

    player completed

--- ofodio/web/urls.py
+++ ofodio/web/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path, include
+from django.urls import include, path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('player/', views.player)
+    # path('player/', views.player),
+    re_path(r'^player/(?P<moviename>\S{0,64})/$', views.player),
 ]
--- ofodio/web/views.py
+++ ofodio/web/views.py
@@ -4,9 +4,14 @@ from .models import Movie
 from django.conf import settings
 
 def home(request):
-    context = {'movies': Movie.objects.all()}
-    return render(request, 'home.html', context)
+	# MM = Movie.objects.all()
+	# for M in MM:
+		# M.file = M.file.split('/')[1]
+		# print(M.file, type(M), type(M.file))
+	context = {'movies': Movie.objects.all()}
+	return render(request, 'home.html', context)
 
-def player(request):
-	context = {'media': settings.MEDIA_URL}
+def player(request, moviename):
+	context = {'media': settings.MEDIA_URL,
+				'moviename':moviename}
 	return render(request, 'player.html', context)
