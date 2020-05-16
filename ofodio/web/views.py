cf3525edb53887dbdf91ed466b12154fa75025d6
mrbayati2002 <mrbayati2002@gmail.com>
2020-05-16 20:08:28 +0430

    player works in "/player"

--- ofodio/web/views.py
+++ ofodio/web/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Movie
+from django.conf import settings
 
 def home(request):
     context = {'movies': Movie.objects.all()}
     return render(request, 'home.html', context)
+
+def player(request):
+	context = {'media': settings.MEDIA_URL}
+	return render(request, 'player.html', context)
